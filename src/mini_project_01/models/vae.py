ed9d199971b0a718a58f9882fa508d899fc317ac
Auðun Bergsson <audun@Auuns-MacBook-Pro.local>
2025-02-28 19:11:56 +0100

    modifying the elbo calculations based on the prior that is used in the VAE

--- src/mini_project_01/models/vae.py
+++ src/mini_project_01/models/vae.py
@@ -2,13 +2,16 @@
 import torch
 import torch.nn as nn
 import torch.distributions as td
-
-
+import hydra
+from helpers import GAUSSIAN, MOG, VAMP
 # Decription: This file contains the implementation of the Variational Autoencoder (VAE) model.
 # The encoder and decoder distributions are defined as separate classes.
 # The prior is defined as a separate in the priors.py file.
 # The VAE model is defined as a class that takes the prior, decoder, and encoder as input.
 
+
+with hydra.initialize(config_path="../../../configs", version_base="1.3"):
+    cfg = hydra.compose(config_name="config.yaml")
 
 class VAE(nn.Module):
     """
@@ -37,7 +40,21 @@ class VAE(nn.Module):
         if hasattr(self.prior, 'encoder'):
             self.prior.encoder = encoder
 
-    def elbo(self, x, num_samples=5):
+    def elbo_gaussian(self, x):
+        """
+        Compute the ELBO for the given batch of data.
+        Parameters:
+        x: [torch.Tensor]
+           A tensor of dimension `(batch_size, feature_dim1, feature_dim2, ...)`
+           n_samples: [int]
+           Number of samples to use for the Monte Carlo estimate of the ELBO.
+        """
+        q = self.encoder(x)
+        z = q.rsample()
+        elbo = torch.mean(self.decoder(z).log_prob(x) - td.kl_divergence(q, self.prior()), dim=0)
+        return elbo
+
+    def elbo_mog(self, x, num_samples=5):
         """
         Compute the ELBO for the given batch of data.
 
@@ -47,6 +64,8 @@ class VAE(nn.Module):
            n_samples: [int]
            Number of samples to use for the Monte Carlo estimate of the ELBO.
         """
+        batch_size = x.size(0)
+        x_reshaped = x.view(batch_size, 28, 28) if x.dim() == 2 else x
         q = self.encoder(x)
         z = q.rsample((num_samples,))
         log_qz = q.log_prob(z)
@@ -54,8 +73,46 @@ class VAE(nn.Module):
 
         kl_mc = torch.mean(log_qz - log_pz, dim = (0,1))
 
-        elbo = torch.mean(self.decoder(z).log_prob(x), dim=(0,1)) - kl_mc
-        
+        elbo = torch.mean(self.decoder(z).log_prob(x_reshaped), dim=(0,1)) - kl_mc
+
+        return elbo
+
+
+    def elbo_vamp(self, x, num_samples=5):
+        """
+        Compute the ELBO for the given batch of data.
+
+        Parameters:
+        x: [torch.Tensor]
+          A tensor of dimension `(batch_size, feature_dim1, feature_dim2, ...)`
+          n_samples: [int]
+          Number of samples to use for the Monte Carlo estimate of the ELBO.
+        """
+
+        batch_size = x.size(0)
+        x_reshaped = x.view(batch_size, 28, 28) if x.dim() == 2 else x
+        q = self.encoder(x)  # batch_shape=[batch_size], event_shape=[latent_dim]
+        z = q.rsample((num_samples,))  # [num_samples, batch_size, latent_dim]
+        log_qz = q.log_prob(z)  # [num_samples, batch_size]
+
+        # Compute the mixture components from pseudo-inputs
+        pseudo_inputs = self.prior.pseudo_inputs  # [K, 784]
+        q_pseudo = self.encoder(pseudo_inputs)  # batch_shape=[K], event_shape=[latent_dim]
+        means = q_pseudo.base_dist.loc  # [K, latent_dim]
+        scales = q_pseudo.base_dist.scale  # [K, latent_dim]
+
+        # Define the mixture distribution
+        mix = td.Categorical(torch.ones(self.prior.K, device=means.device))  # Equal weights, [K]
+        comp = td.Independent(td.Normal(means, scales), 1)  # [K] batch, [latent_dim] event
+        prior_mixture = td.MixtureSameFamily(mix, comp)  # batch_shape=[], event_shape=[latent_dim]
+
+        # Compute log_pz using log_prob
+        log_pz = prior_mixture.log_prob(z)  # [num_samples, batch_size]
+
+        # Compute ELBO
+        kl_mc = torch.mean(log_qz - log_pz, dim=(0, 1))  # scalar
+        elbo = torch.mean(self.decoder(z).log_prob(x_reshaped), dim=(0, 1)) - kl_mc  # scalar
+
         return elbo
 
     def sample(self, n_samples=1):
@@ -83,8 +140,15 @@ class VAE(nn.Module):
             with torch.no_grad():
                 for p1, p2 in zip(self.prior.encoder.parameters(), self.encoder.parameters()):
                     p1.copy_(p2)
-        
-        return -self.elbo(x)
+
+        prior = cfg.priors.name
+        if prior == GAUSSIAN:
+          elbo = self.elbo_gaussian
+        if prior == MOG:
+          elbo = self.elbo_mog
+        if prior == VAMP:
+          elbo = self.elbo_vamp
+        return -elbo(x)
 
 
 class GaussianEncoder(nn.Module):
@@ -114,7 +178,7 @@ class GaussianEncoder(nn.Module):
         """
         mean, std = torch.chunk(self.encoder_net(x), 2, dim=-1)
         return td.Independent(td.Normal(loc=mean, scale=torch.exp(std)), 1)
-    
+
 
 # We consider a binarized mnist, where pixels over 0.5 are considered as 1 and below as 0.
 # Thus, we use a Bernoulli distribution for the decoder.
