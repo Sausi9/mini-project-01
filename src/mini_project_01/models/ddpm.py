215ba6cebaabcfa54fa9db1695ce384f4a64425b
DagurHa <100678896+DagurHa@users.noreply.github.com>
2025-03-03 21:26:07 +0100

    Final ddpm fixes

--- src/mini_project_01/models/ddpm.py
+++ src/mini_project_01/models/ddpm.py
@@ -3,6 +3,7 @@
 import torch
 import torch.nn as nn
 import torch.distributions as td
+import numpy as np
 
 
 class DDPM(nn.Module):
@@ -43,14 +44,11 @@ class DDPM(nn.Module):
         """
 
         ### Implement Algorithm 1 here ###
-        t = torch.randint(high=self.T,size=(1,))
-        t_inp = torch.full(size=(x.shape[0],),fill_value = t[0]/self.T).reshape(-1,1)
-        t_inp = t_inp.to(x.device)
-        eps_dist = td.MultivariateNormal(loc=torch.zeros(x.shape[1]),covariance_matrix=torch.eye(x.shape[1]))
-        eps = eps_dist.sample(sample_shape=(x.shape[0],)).to(x.device)
-        factor = torch.sqrt(1-self.alpha_cumprod[t]).to(x.device)
-        net_input = eps-self.network(torch.sqrt(self.alpha_cumprod[t])*x+factor*eps,t_inp)
-        neg_elbo = torch.norm(net_input,dim=1,p=2)
+        t = torch.randint(1,high=self.T,size=())
+        tnet = torch.full(size=(x.shape[0],1),fill_value = t/self.T)
+        eps = torch.randn(size=x.shape)
+        dist = eps-self.network(torch.sqrt(self.alpha_cumprod[t])*x+torch.sqrt(1-self.alpha_cumprod[t])*eps,tnet)
+        neg_elbo = torch.square(torch.linalg.vector_norm(dist,dim=1,ord=2))
 
         return neg_elbo
 
